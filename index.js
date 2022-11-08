7447d9567bef4186a068edc791b678b07d4d2235
mohamedyasser27 <my2722000@gmail.com>
2022-11-08 16:13:46 +0200

    Add the Campaign Details Section with Animation done using animejs

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import anime from "./anime.es.js";
+
 const DomManipulation = (function () {
   const openMenuBtn = document.querySelector(".openMenuBtn");
   const closeMenuBtn = document.querySelector(".closeMenuBtn");
@@ -16,8 +18,33 @@ const DomManipulation = (function () {
   openMenuBtn.addEventListener("click", openSideMenuEventHandler);
   closeMenuBtn.addEventListener("click", openSideMenuEventHandler);
 
-
-  bookmarkBtn.addEventListener('click', () => {
-    bookmarkBtn.classList.toggle('bookmarked')
-  })
+  bookmarkBtn.addEventListener("click", () => {
+    bookmarkBtn.classList.toggle("bookmarked");
+  });
 })();
+
+let val = 100000;
+let pledges = 89914;
+let backers = 5007;
+let daysLeft = 56;
+let remaining = val - pledges;
+let percentage = (pledges / val) * 100;
+
+let moneyCount = document.querySelector(".moneyCount .currentAmount");
+let backersCount = document.querySelector(".backersCount .currentAmount");
+let percentageBar = document.querySelector(".percentageBar");
+
+window.onload = function () {
+  anime
+    .timeline({
+      duration: 750,
+      easing: "cubicBezier(0.000, 0.000, 0.580, 1.000)",
+      easing: "linear",
+
+      round: 1,
+    })
+    .add({ targets: moneyCount, textContent: `$${[pledges]}` }, 0)
+    .add({ targets: backersCount, textContent: `${backers}` }, 0)
+    .add({ targets: percentageBar, width: `${percentage}%` }, 0);
+};
+
