41416cf1ba742c27f9ad5c19a59367576c45be0f
mohamedyasser27 <my2722000@gmail.com>
2022-11-11 07:25:49 +0200

    Refactor the code with BEM methodology
    first i added the complete payment component
    then redid each block with BEM in mind because the code
    git very messy and unmaintainable

--- index.js
+++ index.js
@@ -2,29 +2,57 @@ import anime from "./anime.es.js";
 
 const DomManipulation = (function () {
   let bookmarked = false;
-  const openMenuBtn = document.querySelector(".openMenuBtn");
-  const closeMenuBtn = document.querySelector(".closeMenuBtn");
-  const sideMenuBtnsContainer = document.querySelector(
-    ".sideMenuBtnsContainer"
+  const sideMenuOpenBtn = document.querySelector(".side-menu__open-btn");
+  const sideMenuCloseBtn = document.querySelector(".side-menu__close-btn");
+  const sideMenu = document.querySelector(".side-menu");
+  let bookmarkStatus = document.querySelector(
+    ".bookmark-area__bookmark-status"
   );
-  let bookmarkStatus = document.querySelector(".bookmarkStatus");
   const cover = document.querySelector(".cover");
-  let bookmarkBtn = document.querySelector(".bookmarkBtn");
+  let projectCardBookmarkBtn = document.querySelector(
+    ".bookmark-area__bookmark-btn"
+  );
   const openSideMenuEventHandler = () => {
     cover.classList.toggle("invisible");
-    sideMenuBtnsContainer.classList.toggle("invisible");
-    openMenuBtn.classList.toggle("invisible");
-    closeMenuBtn.classList.toggle("invisible");
+    sideMenu.classList.toggle("invisible");
+    sideMenuOpenBtn.classList.toggle("invisible");
+    sideMenuCloseBtn.classList.toggle("invisible");
   };
 
-  openMenuBtn.addEventListener("click", openSideMenuEventHandler);
-  closeMenuBtn.addEventListener("click", openSideMenuEventHandler);
+  sideMenuOpenBtn.addEventListener("click", openSideMenuEventHandler);
+  sideMenuCloseBtn.addEventListener("click", openSideMenuEventHandler);
 
-  bookmarkBtn.addEventListener("click", () => {
-    bookmarkBtn.classList.toggle("bookmarked");
+  projectCardBookmarkBtn.addEventListener("click", () => {
+    projectCardBookmarkBtn.classList.toggle("bookmarked");
     bookmarkStatus.textContent = bookmarked ? "bookmark" : "bookmarked";
-    bookmarked=!bookmarked
+    bookmarked = !bookmarked;
   });
+
+  // let product-selection__close-btn = document.querySelector(".product-selection__close-btn");
+  // let product-selection = document.querySelector(".product-selection");
+  // product-selection__close-btn.addEventListener("click", () => {
+  //   product-selection.classList.add("invisible");
+  // });
+
+  // let allProudctsItems = Array.from(
+  //   document.querySelectorAll(".product-selection .available-products__description .product")
+  // );
+
+  // let lastChecked = null;
+  // allProudctsItems.forEach((product) => {
+  //   product.addEventListener("click", function () {
+  //     this.children[0].children[0].checked = true;
+  //     this.classList.add("checked");
+  //     if (lastChecked != null) {
+  //       lastChecked.classList.remove("checked");
+  //       lastChecked = this;
+  //     } else {
+  //       lastChecked = this;
+  //     }
+  //   });
+  // });
+
+  // console.log(allProudctsItems);
 })();
 
 let val = 100000;
@@ -34,9 +62,13 @@ let daysLeft = 56;
 let remaining = val - pledges;
 let percentage = (pledges / val) * 100;
 
-let moneyCount = document.querySelector(".moneyCount .currentAmount");
-let backersCount = document.querySelector(".backersCount .currentAmount");
-let percentageBar = document.querySelector(".percentageBar");
+let moneyCount = document.querySelector(
+  ".card__counter--money .counter__currentAmount"
+);
+let backersCount = document.querySelector(
+  ".card__counter--backers-count .counter__currentAmount"
+);
+let percentageBar = document.querySelector(".progress-bar__done-progress");
 
 let options = {
   root: null,
@@ -60,6 +92,12 @@ const callback = function (entries) {
     observer.unobserve(target);
   }
 };
-let target = document.querySelector(".campaignDetails");
+let target = document.querySelector(".campaign-card");
 let observer = new IntersectionObserver(callback, options);
 observer.observe(target);
+
+/**
+ * 1)
+ *
+ *
+ */
